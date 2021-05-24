3852cb708d1a3ee163f9181d3422abf3cdf3ee30
Hyunwoo Cho <hcho@c10r1s2.42seoul.kr>
2021-05-25 03:57:40 +0900

    aa

--- d01/ex03/array2hash_sorted.php
+++ d01/ex03/array2hash_sorted.php
@@ -4,7 +4,7 @@ function array2hash_sorted($array)
 	$newarray = array();
 	foreach($array as $arr)
 		$newarray[$arr[0]] = $arr[1];
-	asort($newarray);
+	krsort($newarray);
 	return $newarray;
 }
 ?>
